cd107662c71e66688fe710b99f5507b108a8c1df
Daniel <danielbb2000@gmail.com>
2021-10-05 17:58:05 -0300

    refactor: move all error import to a single file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,8 @@
-import { ServerError } from './../errors/server-error'
-import { InvalidParamError } from './../errors/invalid-param-error'
 import { HttpRequest, HttpResponse } from './../protocols/http'
-import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest, serverBadRequest } from '../Helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { emailValidator } from '../protocols/email-validator'
+import { InvalidParamError, MissingParamError, ServerError } from '../errors/index'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: emailValidator
--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -1,8 +1,6 @@
-import { InvalidParamError } from './../errors/invalid-param-error'
-import { MissingParamError } from './../errors/missing-param-error'
 import { SignUpController } from './signup'
 import { emailValidator } from '../protocols/email-validator'
-import { ServerError } from '../errors/server-error'
+import { InvalidParamError, MissingParamError, ServerError } from '../errors/index'
 
 interface SutTypes{
   sut: SignUpController
